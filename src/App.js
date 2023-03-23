a06877149008af1a5b00972f0749e81663f9e4c8
Chirag Dogra <chirag.dogra79@gmail.com>
2023-03-23 21:27:24 +0530

    react router dom is working properly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import About from './pages/About';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar'
-import Home from './pages/Home';
-import HomePage from './pages/HomePage';
-import Services from './pages/Services';
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
+import Services from "./pages/Services";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <HomePage/>
-      {/* <Home/> */}
-      {/* <About /> */}
-      {/* <Services/> */}
-
-      <Footer /> 
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="home" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="services" element={<Services />}></Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
-import logo from '../assets/logo.png'
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
@@ -13,38 +13,51 @@ const Navbar = () => {
   return (
     <div className="w-screen h-[70px] z-10 bg-zinc-200 fixed drop-shadow-lg z-50">
       <div className="px-2 flex justify-between items-center w-full h-full">
-        <div className="flex items-center">
-        <img src={logo} width={160} height={160} className="laptop:ml-6 mob:ml-1"/>
-        </div>
-        <div className="relative float-right ">
-            <ul className="hidden md:flex m-9">
-              <li className="transition-all ease-out duration-300 hover:scale-110 hover: cursor-pointer">
-                <Link to="home" smooth={true} duration={500}>
-                  Home
-                </Link>
-              </li>
-              <li className="transition-all ease-out duration-300 hover:scale-110 hover: cursor-pointer">
-                <Link to="about" smooth={true} offset={-200} duration={500}>
-                  About us
-                </Link>
-              </li>
-              <li className="transition-all ease-out duration-300 hover:scale-110 hover: cursor-pointer">
-                <Link to="support" smooth={true} offset={-50} duration={500}>
-                  Sevices
-                </Link>
-              </li>
-              <li className="transition-all ease-out duration-300 hover:scale-110 hover: cursor-pointer">
-                <Link to="platforms" smooth={true} offset={-100} duration={500}>
-                  Blog
-                </Link>
-              </li>
-              <li className="transition-all ease-out duration-300 hover:scale-105 hover: cursor-pointer">
-                <Link to="pricing" smooth={true} offset={-50} duration={500} className = "bg-indigo-700 transition-all ease-out duration-300 hover:scale-105 text-white p-2 pb-2.5 rounded-md">
-                  Contact
-                </Link>
-              </li>
-            </ul>
+        <NavLink to="/">
+          <div className="flex items-center hover:cursor-pointer">
+            <img
+              src={logo}
+              width={160}
+              height={160}
+              className="laptop:ml-6 mob:ml-1"
+            />
           </div>
+        </NavLink>
+        <div className="relative float-right ">
+          <ul className="hidden md:flex m-9">
+            <li className="transition-all ease-out duration-300 hover:scale-110 hover: cursor-pointer">
+              <NavLink to="home" smooth={true} duration={500}>
+                Home
+              </NavLink>
+            </li>
+            <li className="transition-all ease-out duration-300 hover:scale-110 hover: cursor-pointer">
+              <NavLink to="about" smooth={true} offset={-200} duration={500}>
+                About us
+              </NavLink>
+            </li>
+            <li className="transition-all ease-out duration-300 hover:scale-110 hover: cursor-pointer">
+              <NavLink to="services" smooth={true} offset={-50} duration={500}>
+                Sevices
+              </NavLink>
+            </li>
+            <li className="transition-all ease-out duration-300 hover:scale-110 hover: cursor-pointer">
+              <NavLink to="/" smooth={true} offset={-100} duration={500}>
+                Blog
+              </NavLink>
+            </li>
+            <li className="transition-all ease-out duration-300 hover:scale-105 hover: cursor-pointer">
+              <NavLink
+                to="pricing"
+                smooth={true}
+                offset={-50}
+                duration={500}
+                className="bg-indigo-700 transition-all ease-out duration-300 hover:scale-105 text-white p-2 pb-2.5 rounded-md"
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
         <div className="md:hidden mr-4" onClick={handleClick}>
           {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
         </div>
@@ -52,12 +65,12 @@ const Navbar = () => {
 
       <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link onClick={handleClose} to="home" smooth={true} duration={500}>
+          <NavLink onClick={handleClose} to="home" smooth={true} duration={500}>
             Home
-          </Link>
+          </NavLink>
         </li>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link
+          <NavLink
             onClick={handleClose}
             to="about"
             smooth={true}
@@ -65,10 +78,10 @@ const Navbar = () => {
             duration={500}
           >
             About us
-          </Link>
+          </NavLink>
         </li>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link
+          <NavLink
             onClick={handleClose}
             to="support"
             smooth={true}
@@ -76,10 +89,10 @@ const Navbar = () => {
             duration={500}
           >
             Services
-          </Link>
+          </NavLink>
         </li>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link
+          <NavLink
             onClick={handleClose}
             to="platforms"
             smooth={true}
@@ -87,10 +100,10 @@ const Navbar = () => {
             duration={500}
           >
             Blog
-          </Link>
+          </NavLink>
         </li>
         <li className="border-b-2 border-zinc-300 w-full bg-indigo-700  text-white p-2 pb-2.5 rounded-md m-2">
-          <Link
+          <NavLink
             onClick={handleClose}
             to="pricing"
             smooth={true}
@@ -98,7 +111,7 @@ const Navbar = () => {
             duration={500}
           >
             Contact
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </div>
